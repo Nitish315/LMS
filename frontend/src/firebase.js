3395be8c6a3277cad701c18f82299b11f6ef57ea
Nitish315 <149555181+Nitish315@users.noreply.github.com>
2026-05-20 21:52:25 +0530

    initial Commit

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,34 +1,21 @@
-// import { initializeApp, getApps, getApp } from "firebase/app";
-// import { getAuth, GoogleAuthProvider } from "firebase/auth";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// const firebaseConfig = {
-//   apiKey: "YOUR_API_KEY",
-//   authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-//   projectId: "YOUR_PROJECT_ID",
-//   storageBucket: "YOUR_PROJECT_ID.appspot.com",
-//   messagingSenderId: "XXXX",
-//   appId: "XXXX",
-// };
-
-// // ✅ THIS LINE IS THE FIX
-// const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
-
-// export const auth = getAuth(app);
-// export const provider = new GoogleAuthProvider();
-import { initializeApp, getApps, getApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
-
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAnYLq49KCHtDvumfM7KS4g2J3ks7wpuyQ",
   authDomain: "lmsproject-8a57c.firebaseapp.com",
   projectId: "lmsproject-8a57c",
-  storageBucket: "lmsproject-8a57c.appspot.com",
+  storageBucket: "lmsproject-8a57c.firebasestorage.app",
   messagingSenderId: "211042481273",
   appId: "1:211042481273:web:d03a82e54394fc76e150a8",
-  measurementId: "G-S6G3WH9YJS",
+  measurementId: "G-S6G3WH9YJS"
 };
 
-const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
-
-export const auth = getAuth(app);
-export const provider = new GoogleAuthProvider();
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
